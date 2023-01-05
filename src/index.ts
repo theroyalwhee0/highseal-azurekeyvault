364ca4fcfe59cd617473a296e7e585ffce5f9436
Adam Mill <hismajesty@theroyalwhee.com>
2023-01-05 13:52:35 -0500

    Create basic project.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { HighsealCLIExtension } from '@theroyalwhee0/highseal-ext';
+
+/**
+ * Export symbol indicating this is a Highseal CLI Extension.
+ */
+export const HighsealCLIExt = HighsealCLIExtension;
